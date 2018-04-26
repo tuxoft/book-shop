f417da17e8af933aabbf960be34a2ca7b7c51bb6
Zorigto Sambyalov <samzorik@gmail.com>
2018-04-26 17:08:16 +0800

    #3 Добавить экран корзины.

--- client-angular/src/app/modules/cart/components/cart-page/cart-page.component.ts
+++ client-angular/src/app/modules/cart/components/cart-page/cart-page.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-cart-page',
   templateUrl: './cart-page.component.html',
-  styleUrls: ['./cart-page.component.css'],
+  styleUrls: ['./cart-page.component.scss'],
 })
 export class CartPageComponent implements OnInit {
 
