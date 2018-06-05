cb74ed5d089eaa67b173e657ac789badd8e52569
Vyacheslav Kokorin <k-v-p@yandex.ru>
2018-06-05 15:34:57 +0800

    user

--- server-node-express/src/app/orm/entity/user.ts
+++ server-node-express/src/app/orm/entity/user.ts
@@ -14,4 +14,16 @@ export class User {
 
   @OneToMany(type => Order, order => order.user, { eager: true })
   orders: Order[];
+
+  // Название учетной записи хранится в keycloak
+  userName?: string;
+
+  // Имя пользователя хранятся в keycloak
+  firstName?: string;
+
+  // Фамилия пользователя хранятся в keycloak
+  lastName?: string;
+
+  // Адрес электронной почты пользователя хранится в keycloak
+  email?: string;
 }
